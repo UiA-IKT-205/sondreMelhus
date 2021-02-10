69f7163587938e71fd01b9a896396132cec7a64f
semelhus <semelhus@uia.no>
2021-02-10 17:26:34 +0100

    la til tidsknapper

--- app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -8,15 +8,17 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import no.uia.ikt205.pomodoro.util.millisecondsToDescriptiveTime
+import no.uia.ikt205.pomodoro.util.minutesToMilliSeconds
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var timer:CountDownTimer
     lateinit var startButton:Button
-    lateinit var coutdownDisplay:TextView
+    lateinit var countdownDisplay:TextView
 
-    val timeToCountDownInMs = 5000L
+    var timeToCountDownInMs = 5000L
     val timeTicks = 1000L
+    private val buttonTimeIncrement = 30L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +28,33 @@ class MainActivity : AppCompatActivity() {
        startButton.setOnClickListener(){
            startCountDown(it)
        }
-       coutdownDisplay = findViewById<TextView>(R.id.countDownView)
+       countdownDisplay = findViewById<TextView>(R.id.countDownView)
 
+        val setTimerDurationButtons = listOf<Button>(findViewById<Button>(R.id.setTimerDuration30MinutesButton),
+                findViewById<Button>(R.id.setTimerDuration60MinutesButton),
+                findViewById<Button>(R.id.setTimerDuration90MinutesButton),
+                findViewById<Button>(R.id.setTimerDuration120MinutesButton)
+        )
+
+        setTimerDurationButtons.forEachIndexed(){ index, button ->
+            button.setOnClickListener(){
+                val newCountdownTime = minutesToMilliSeconds((index+1) * buttonTimeIncrement)
+                setCountdownTime(newCountdownTime)
+
+            }
+        }
     }
 
-    fun startCountDown(v: View){
+    private fun setCountdownTime(newCountdownTimeInMs:Long){
+        timeToCountDownInMs =  newCountdownTimeInMs
+        updateCountDownDisplay(timeToCountDownInMs)
+    }
+
+    private fun startCountDown(viewOfTimer: View){
 
         timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
             override fun onFinish() {
+                viewOfTimer.isEnabled = true
                 Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
             }
 
@@ -41,12 +62,12 @@ class MainActivity : AppCompatActivity() {
                updateCountDownDisplay(millisUntilFinished)
             }
         }
-
+        viewOfTimer.isEnabled = false
         timer.start()
     }
 
     fun updateCountDownDisplay(timeInMs:Long){
-        coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
+        countdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
     }
 
 }
